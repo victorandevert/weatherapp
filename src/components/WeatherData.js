899793aceea544b2855504d487639836e438e215
zhafi <victor.andevert@gmail.com>
2018-02-17 19:07:25 +0100

    add temperature and extra info components

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import WeatherTemperature from './WeatherTemperature';
+import WeatherExtraInfo from './WeatherExtraInfo';
 
 const WeatherData = () => (
-  <div>WeatherData</div>
+  <div>
+      <WeatherTemperature/>
+      <WeatherExtraInfo/>
+  </div>
 );
 
 export default WeatherData;
---
+++ src/components/WeatherTemperature.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WeatherTemperature = () => (
+  <div><span>12 Cº</span></div>
+);
+
+export default WeatherTemperature;
