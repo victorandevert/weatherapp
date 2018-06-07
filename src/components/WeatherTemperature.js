a35923743d570a57ed9ca76dc4c7f38c190e7022
zhafi <victor.andevert@gmail.com>
2018-06-07 11:47:50 +0200

    add cloud icon

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
 
 const WeatherTemperature = () => (
-  <div><span>12 Cº</span></div>
+  <div>
+      <WeatherIcons name="sleet" size="2x" />
+      <span>12 Cº</span>
+  </div>
 );
 
 export default WeatherTemperature;
